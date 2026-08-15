1311f3d78c6ec12afe863bcad50d305c4b1286b1
alexgower <alexanderpgower@gmail.com>
2026-08-15 00:09:04 +0000

    Deploying to gh-pages from @ alexgower/alexgower.github.io@80187a5b3313766a743e75ee89275950f7163d79 🚀

---
+++ assets/js/zoom.js
@@ -0,0 +1,1 @@
+document.addEventListener("DOMContentLoaded",function(){mediumZoom("[data-zoomable]",{background:getComputedStyle(document.documentElement).getPropertyValue("--global-bg-color")+"ee"})});
